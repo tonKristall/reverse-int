a956042855153715b2ca28732c7b062723431f02
tonKristall <krisale@yandex.ru>
2021-01-28 22:20:41 +0300

    feat: task pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = -n;
+  }
+  let str = String(n);
+  let strarr = str.split("");
+  let newstr = [];
+  for (i = strarr.length - 1; i >= 0; i--) {
+    newstr.push(strarr[i]);
+  }
+  newn = newstr.join("");
+  return newn;
 }
